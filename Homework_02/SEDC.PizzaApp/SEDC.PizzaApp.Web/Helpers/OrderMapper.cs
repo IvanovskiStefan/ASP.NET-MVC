f1299bc4a14dd586a2a259311c8872b5d366938e
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2022-06-29 15:19:09 +0200

    UpdatePizza
    Update on homeworkPizzaApp

---
+++ Homework_02/SEDC.PizzaApp/SEDC.PizzaApp.Web/Helpers/OrderMapper.cs
@@ -0,0 +1,54 @@
+﻿using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Web.Helpers
+{
+    public static class OrderMapper
+    {
+        public static OrderListViewModel MapToOrderListViewModel(this Order order)
+        {
+            return new OrderListViewModel
+            {
+                PizzaName = order.Pizza.Name,
+                UserFullName = $"{order.User.FirstName} {order.User.LastName}"
+            };
+        }
+
+        public static OrderDetailsViewModel MapToOrderDetailsViewModel(this Order order)
+        {
+            return new OrderDetailsViewModel
+            {
+                Id = order.Id,
+                PizzaName = order.Pizza.Name,
+                UserFullName = $"{order.User.FirstName} {order.User.LastName}",
+                PaymentMethod = order.PaymentMethod,
+                Price = order.Pizza.IsOnPromotion ? 200 : order.Pizza.Price,
+                IsDelivered = order.IsDelivered
+            };
+        }
+
+        public static Order MapToOrder(this OrderViewModel orderViewModel)
+        {
+            return new Order
+            {
+                Id = orderViewModel.Id,
+                PaymentMethod = orderViewModel.PaymentMethod,
+                User = StaticDb.Users.SingleOrDefault(x => x.Id == orderViewModel.UserId),
+                IsDelivered = false,
+                Pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Name == orderViewModel.PizzaName)
+            };
+        }
+
+        //public static Order MapToEdit(this Order order)
+        //{
+        //    return new Order
+        //    {
+        //        Id = order.Id,
+        //        PaymentMethod = order.PaymentMethod,
+        //        User = order.User,
+        //        IsDelivered = order.IsDelivered,
+        //        Pizza = order.Pizza,
+        //    };
+        //}
+    }
+}
---
+++ Homework_02/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/ViewModels/EditViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SEDC.PizzaApp.Web.Models.ViewModels
+{
+    public class EditViewModel
+    {
+        public int OrderId { get; set; }
+        public string FullName { get; set; }
+        public string PizzaName { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
